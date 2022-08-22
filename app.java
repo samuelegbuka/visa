37a2890bd4ce8472a98e39c1d78e15cc4e5aa0ba
MrEgbuka <108094774+MrEgbuka@users.noreply.github.com>
2022-08-22 13:04:29 +0100

    Car servicing feature added

---
+++ app.java
@@ -0,0 +1,10 @@
+print("simon")
+print("Ivo")
+# echo simon
+print("chidi")
+print("grace")
+print("glory")
+print("LSS")
+# bugs removed
+print("car services automaated")
+print("Hello $user, your car need servicing")
